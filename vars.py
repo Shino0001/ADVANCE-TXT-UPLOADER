0312b7b4898b2b683928c4edc4d4c6d102bc58df
Shino0001 <162863222+Shino0001@users.noreply.github.com>
2025-04-16 13:51:55 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,4 +5,4 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "27049009")) #Replace with your api id
 API_HASH = environ.get("API_HASH", "fdbb025703492ee68251614682e2b4d3") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "7316725152:AAFBkBzfykHGj2goLV4H17ZqvN8BxfcCyfg") #Replace with your bot token
+BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
