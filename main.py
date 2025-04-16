eadcfd5724e4ca0f062cc2d147af1cd23df3cbc3
Shino0001 <162863222+Shino0001@users.noreply.github.com>
2025-04-16 23:02:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ async def show_random_emojis(message):
 OWNER_ID = 7541753640 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
-SUDO_USERS = [5840594311,7856557198,6303334633]
+SUDO_USERS = []
 
 AUTH_CHANNEL = -1002572301679
 
